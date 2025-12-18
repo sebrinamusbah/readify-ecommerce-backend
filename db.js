7610511bcd4b3b4c872eb4175a1d51f707493a56
sebrinamusbah <sebrinm9@gmail.com>
2025-12-18 16:50:49 +0300

    update db

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-// db.js
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false, // needed for Render PostgreSQL
+        rejectUnauthorized: false,
     },
 });
 
